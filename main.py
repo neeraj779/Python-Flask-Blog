a778745925d5892efea55f19e0babcf9991d49d1
Neeraj <neeraj9108@gmail.com>
2021-07-12 19:01:56 +0530

    added configurable data support

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template , request
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
@@ -13,6 +13,7 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = data["production_uri"]
 db = SQLAlchemy(app)
 
+
 class Contact(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
@@ -21,27 +22,32 @@ class Contact(db.Model):
     date = db.Column(db.String(12), nullable=True)
     email = db.Column(db.String(20), nullable=False)
 
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", data=data)
+
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
-    
+    return render_template("about.html", data=data)
+
+
 @app.route("/contact", methods=['GET', 'POST'])
 def contact():
-    if(request.method=='POST'):
+    if(request.method == 'POST'):
         '''Add entry to the database'''
         name = request.form.get('name')
         email = request.form.get('email')
         phone = request.form.get('phone')
         message = request.form.get('message')
 
-        entry = Contact(name=name, date = datetime.now(), phone_num=phone, msg=message, email=email)
+        entry = Contact(name=name, date=datetime.now(),
+                        phone_num=phone, msg=message, email=email)
         db.session.add(entry)
         db.session.commit()
 
+    return render_template("contact.html", data=data)
 
-    return render_template("contact.html")
+
 app.run(debug=True)
