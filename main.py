75992d5eb883348ea5741866fcf0d67841fe3026
Neeraj <neeraj9108@gmail.com>
2021-07-12 15:24:20 +0530

    added database support

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/my blog'
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    phone_num = db.Column(db.String(15), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
 
 @app.route("/")
 def home():
@@ -10,7 +22,19 @@ def home():
 def about():
     return render_template("about.html")
     
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
+    if(request.method=='POST'):
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+        entry = Contact(name=name, date = datetime.now(), phone_num=phone, msg=message, email=email)
+        db.session.add(entry)
+        db.session.commit()
+
+
     return render_template("contact.html")
 app.run(debug=True)
